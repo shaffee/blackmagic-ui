216de1356cdad7ed53375c7ab89284524a1e1374
Shaffee <shaffee.mayoof@scriptbh.com>
2021-08-17 19:53:12 +0300

    fixes

--- backend/build.sh
+++ backend/build.sh
@@ -8,8 +8,10 @@ git pull
 npm install
 rm -R /tmp/production_tmp
 mkdir -p /tmp/production_tmp
-mkdir -p $PWD/../production
+
 
 ng config -g cli.warnings.versionMismatch false
 ng build --prod --output-path=/tmp/production_tmp --source-map=false
+rm -R $PWD/../production/
+mkdir -p $PWD/../production
 mv /tmp/production_tmp/* $PWD/../production/
